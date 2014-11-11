d4e3520e8a9fa109727296515be9995ddf9d96fb
Chris Slamar <chris@slamar.com>
2014-11-11 17:50:57 -0600

    Basic functionality

--- handbrake-batch-convert.py
+++ handbrake-batch-convert.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import sys
 import os
 import argparse
 import re
@@ -11,22 +10,25 @@ def buildSourceQueue(source_files):
         for source_vid in in_file:
             file_name, file_extension = os.path.splitext(source_vid)
             if video_extensions.__contains__(file_extension):
-                print('DEBUG: ' + source_vid)
-                files_queue.append(source_vid)
+                # print('DEBUG: ' + source_files + '/' + source_vid)
+                files_queue.append((source_files, source_vid))
 
-    print('')
+    # print('')
     return files_queue
 
 
 def listdirFullPath(path_to_file):
-    return (os.path.dirname(path_to_file), os.listdir(path_to_file))
+    # output_path_to_dir = os.path.dirname(path_to_file)
+    output_path_to_dir = path_to_file
+    output_path_to_file = os.listdir(path_to_file)
+    return (output_path_to_dir, output_path_to_file)
 
 
 def createConversionQueue(videos_list):
     scrubbed_names = []
-    for name in videos_list:
+    for dir, name in videos_list:
         file_name, file_extension = os.path.splitext(name)
-        scrubbed_names.append((re.escape(name), re.escape(file_name) + default_extension))
+        scrubbed_names.append((dir + '/' + re.escape(name), re.escape(file_name) + default_extension))
 
     return scrubbed_names
 
@@ -47,18 +49,22 @@ default_extension = '.m4v'
 convert_queue = buildSourceQueue(args.source)
 output_queue = createConversionQueue(convert_queue)
 
-print('Converting following files to ' + args.output)
-print('----')
+# print('Converting following files to ' + args.output)
+# print('----')
+#
+# for f in convert_queue:
+#     print('DEBUG: ' + str(f))
+# print('----')
+#
+# print('Scrubbed names are:')
+#
+# for f in output_queue:
+#     print('DEBUG: ' + str(f))
+# print('----')
 
-for f in convert_queue:
-    print('DEBUG: ' + f)
-print('----')
-
-print('Scrubbed names are:')
-
-for f in output_queue:
-    print('DEBUG: ' + f[1])
-print('----')
 
 for source_file, output_file in output_queue:
-    print(handbrake_path + ' -v -Z \'' + preset + '\' -i ' + source_file + ' -o ' + args.output + '/' + output_file)
+    print('Running...')
+    run_command = handbrake_path + ' -v -Z \'' + preset + '\' -i ' + source_file + ' -o ' + args.output + '/' + output_file
+    print(run_command)
+    os.system(run_command)
