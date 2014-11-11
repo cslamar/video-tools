29e79418f76b7db02e48316e1d9365496fbed089
Chris Slamar <chris@slamar.com>
2014-11-11 15:19:07 -0600

    Refactored naming functions to return tuples of paths and files

--- handbrake-batch-convert.py
+++ handbrake-batch-convert.py
@@ -2,36 +2,31 @@
 import sys
 import os
 import argparse
-from re import escape
+import re
 
 
 def buildSourceQueue(source_files):
     files_queue = []
-    for in_file in testList(source_files):
+    for in_file in listdirFullPath(source_files):
         for source_vid in in_file:
             file_name, file_extension = os.path.splitext(source_vid)
             if video_extensions.__contains__(file_extension):
-                print(source_vid)
-                files_queue.append(in_file)
+                print('DEBUG: ' + source_vid)
+                files_queue.append(source_vid)
 
+    print('')
     return files_queue
 
 
-
-def testList(path_to_file):
-
-    return (os.path.dirname(path_to_file), os.listdir(path_to_file))
-
-
 def listdirFullPath(path_to_file):
-    # return [os.path.join(path_to_file, f) for f in os.listdir(path_to_file)]
     return (os.path.dirname(path_to_file), os.listdir(path_to_file))
 
 
-def scrubNames(videos_list):
+def createConversionQueue(videos_list):
     scrubbed_names = []
     for name in videos_list:
-        scrubbed_names.append(escape(name))
+        file_name, file_extension = os.path.splitext(name)
+        scrubbed_names.append(re.escape(file_name) + default_extension)
 
     return scrubbed_names
 
@@ -39,7 +34,7 @@ def scrubNames(videos_list):
 parser = argparse.ArgumentParser(description='HandBrake Conversion Tool!')
 parser.add_argument('-s', '--source', help='path to source directory [default: current directory]', default=os.path.abspath(os.curdir))
 parser.add_argument('-o', '--output', help='path to output directory', required=True)
-parser.add_argument('-R', '--recursion', help='recursively select all video files in file tree', default=False)
+# parser.add_argument('-R', '--recursion', help='recursively select all video files in file tree', default=False)
 args = parser.parse_args()
 
 video_extensions = ('.mkv', '.avi', '.mp4', '.m4v', '.flv', '.mov')
@@ -52,27 +47,25 @@ default_extension = '.m4v'
 # print args.output
 
 
-print('#####')
-buildSourceQueue(args.source)
-print('#####')
+convert_queue = buildSourceQueue(args.source)
+
+print('Converting following files to ' + args.output)
+print('----')
+
+for f in convert_queue:
+    print('DEBUG: ' + f)
+#
+print('----')
+output_queue = createConversionQueue(convert_queue)
+
+print('Scrubbed names are:')
+
+for f in output_queue:
+    print('DEBUG: ' + f)
+
+print('----')
 
 
-# convert_queue = buildSourceQueue(args.source)
-#
-# print('Converting following files to ' + args.output)
-#
-# for f in convert_queue:
-#     print(f)
-#
-# print('----')
-# output_queue = scrubNames(convert_queue)
-#
-# print('Scrubbed names are:')
-#
-# for f in output_queue:
-#     print(f)
-#
-# print('----')
 
 # for source_video in convert_queue:
 #     print(handbrake_path + ' -v -Z \'' + preset + '\' -i ' + source_video + ' -o ' + args.output)
